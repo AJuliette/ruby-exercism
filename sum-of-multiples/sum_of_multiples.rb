f455c876f9790ae5589268bfc486cc9149d47ec5
AJuliette <42354334+AJuliette@users.noreply.github.com>
2025-04-11 12:08:10 +0200

    Complete 45 sum of multiples (#80)

---
+++ sum-of-multiples/sum_of_multiples.rb
@@ -0,0 +1,15 @@
+class SumOfMultiples
+  def initialize(*base_value_of_magical_items)
+    @base_value_of_magical_items = base_value_of_magical_items
+  end
+
+  def to(level_completed)
+    @base_value_of_magical_items.flat_map do |base_value_of_magical_item|
+      return 0 if base_value_of_magical_item.zero?
+      range = (level_completed % base_value_of_magical_item).zero? ? (1...(level_completed/base_value_of_magical_item)) : (1..(level_completed/base_value_of_magical_item))
+      range.map do |n|
+        n * base_value_of_magical_item
+      end
+    end.uniq.sum
+  end
+end
