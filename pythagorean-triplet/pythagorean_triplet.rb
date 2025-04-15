851023690eac4c26a81ee6b5307a6e5acd5edb19
Juliette Audema <juliette.audema@gmail.com>
2025-04-15 12:16:15 +0200

    Refine 55 pythagorean triplet

--- pythagorean-triplet/pythagorean_triplet.rb
+++ pythagorean-triplet/pythagorean_triplet.rb
@@ -2,11 +2,10 @@ class PythagoreanTriplet
   def self.triplets_with_sum(sum)
     triplets = []
     (1..sum / 3).each do |a|
-      ((a+1)..sum / 2).each do |b|
-        c = sum - a - b
-        triplets.push([a, b, c]) if a < b && b < c && (a**2 + b**2) == c**2
-      end
+      b = (sum**2 - 2 * sum * a) / (2 * sum - 2 * a)
+      c = sum - a - b
+      triplets.push([a, b, c].sort) if (a**2 + b**2) == c**2
     end
-    triplets
+    triplets.uniq
   end
 end
