24a734091ad992081281ef80121551d26b7e8065
Juliette Audema <juliette.audema@gmail.com>
2025-04-15 10:39:45 +0200

    Refine 52 saddle points

--- saddle-points/saddle_points.rb
+++ saddle-points/saddle_points.rb
@@ -1,24 +1,25 @@
 class Grid
   def self.saddle_points(section)
-    section.each_with_object([]).with_index do |(row, array), row_index|
+    transpose = section.transpose
+    section.each_with_object([]).with_index do |(row, coordinates), row_index|
       row.each_with_index do |tree, column_index|
-        array << { 'row' => (row_index + 1), 'column' => (column_index + 1) } if is_a_good_tree?(section:, row:, column: column_index, tree:)
+        coordinates << { 'row' => (row_index + 1), 'column' => (column_index + 1) } if is_a_good_tree?(transpose:, row:, column_index:, tree:)
       end
     end
   end
 
   private
 
-  def self.is_a_good_tree?(section:, row:, column:, tree:)
+  def self.is_a_good_tree?(transpose:, row:, column_index:, tree:)
     is_taller_than_every_tree_to_the_east_and_west?(row:, tree:) &&
-      is_shorter_than_every_tree_to_the_north_and_south(section:, column:, tree:)
+      is_shorter_than_every_tree_to_the_north_and_south(transpose:, column_index:, tree:)
   end
 
   def self.is_taller_than_every_tree_to_the_east_and_west?(row:, tree:)
-    row.none? { |other_tree| other_tree > tree }
+    tree == row.max
   end
 
-  def self.is_shorter_than_every_tree_to_the_north_and_south(section:, column:, tree:)
-    section.none? { |row| row[column] < tree }
+  def self.is_shorter_than_every_tree_to_the_north_and_south(transpose:, column_index:, tree:)
+    tree == transpose[column_index].min
   end
 end
