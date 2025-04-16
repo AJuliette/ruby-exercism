8581cd9a9fc67f8b6b1c3fa96288231f80b7523d
Juliette Audema <juliette.audema@gmail.com>
2025-04-16 10:21:19 +0200

    Refine 58 proverb

--- proverb/proverb.rb
+++ proverb/proverb.rb
@@ -5,17 +5,12 @@ class Proverb
   end
 
   def to_s
-    length = @words.length
-    @words.each_with_index.map do |word, index|
-      if index != (length - 1)
-        "For want of a #{ word } the #{ @words[index + 1] } was lost."
-      else
-        if @qualifier
-          "And all for the want of a #{ @qualifier } #{ @words[0] }."
-        else
-          "And all for the want of a #{ @words[0] }."
-        end
-      end
-    end.join("\n")
+    lines = []
+    @words.each_cons(2) do |first, second|
+      lines.push("For want of a #{ first } the #{ second } was lost.")
+    end
+    first_item = @qualifier ? "#{ @qualifier } #{ @words.first }" : @words.first
+    lines.push("And all for the want of a #{ first_item }.")
+    lines.join("\n")
   end
 end
