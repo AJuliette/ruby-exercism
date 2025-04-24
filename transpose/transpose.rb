72c6d45954c89cec6ab9d0241bbb63a51ec83d05
Juliette Audema <juliette.audema@gmail.com>
2025-04-24 12:24:43 +0200

    Resolve 46 transpose

--- transpose/transpose.rb
+++ transpose/transpose.rb
@@ -1,35 +1,40 @@
 class Transpose
   def self.transpose(input)
-    rows = input.split("\n")
-    columns = rows.map do |row|
-      transpose_row_in_column(row)
-    end
-    join_columns(columns)
+    rows = build_rows(input)
+    columns = build_columns(rows)
+    columns.join("\n")
   end
 
-  def self.transpose_row_in_column(row)
-    columns = {}
-    row.chars.each_with_index do |letter, index|
-      columns[index] ||= ''
-      columns[index] += letter
-    end
-    rows = []
-    columns.map do |column|
-      index, string = column
-      rows[index] ||= ''
-      rows[index] += string
-    end
-    rows.join("\n")
+  private
+
+  def self.build_rows(input)
+    input.split(/\n/).map { |string| string.chars }
   end
 
-  def self.join_columns(columns)
-    rows = {}
-    columns.map do |column|
-      column.split("\n").each_with_index do |letter, index|
-        rows[index] ||= ''
-        rows[index] += letter
-      end
+  def self.build_columns(rows)
+    padded_rows = pad_rows(rows)
+    columns = padded_rows.transpose
+    trimmed_columns = trim_columns(columns)
+    pad_columns(trimmed_columns)
+  end
+
+  def self.pad_rows(rows)
+    rows.each_with_index.map { |row, index| rows[index] += [' '] * (max_length(rows) - rows[index].size) }
+  end
+
+  def self.trim_columns(columns)
+    columns.map.with_index do |column, index|
+      column.join.rstrip
     end
-    rows.values.join("\n")
+  end
+
+  def self.pad_columns(columns)
+    columns.each_with_index do |column, index| 
+      (max_length(columns[index..-1]) - column.length).times { column << ' ' }
+    end
+  end
+
+  def self.max_length(arrays)
+    arrays.max_by(&:length).length
   end
 end
